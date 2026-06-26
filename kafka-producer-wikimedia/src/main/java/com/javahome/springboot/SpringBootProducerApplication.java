1bc112744e392a18e63f188f97d13b4bded0846a
Sonal Tamse <sonal.tamse@gmail.com>
2026-06-26 21:37:48 +0530

    feat: add Kafka producer module

---
+++ kafka-producer-wikimedia/src/main/java/com/javahome/springboot/SpringBootProducerApplication.java
@@ -0,0 +1,11 @@
+package com.javahome.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProducerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProducerApplication.class, args);
+    }
+}
